5be30ec68d55d522e8a30b02d6eca9433c2bdc7e
Salvador Briones <mexidense@gmail.com>
2023-11-27 16:13:40 +0100

    feat: set homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import CodeGeneration from "@uml2code/view/code-generation/code-generation";
+import {Container} from "@mui/material";
 
 export default function Home() {
     return (
     <main>
-        <CodeGeneration/>
+        <Container>
+            <CodeGeneration/>
+        </Container>
     </main>
   )
 }
