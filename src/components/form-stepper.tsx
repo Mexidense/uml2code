5e9e4b8168a62a63a0ca6d8c216987e7901daf93
Salvador Briones <mexidense@gmail.com>
2023-11-28 21:21:33 +0100

    refactor: decouple setup step

--- src/components/form-stepper.tsx
+++ src/components/form-stepper.tsx
@@ -8,37 +8,9 @@ import Typography from '@mui/material/Typography';
 import {ImageFormStep} from "@uml2code/components/form-steps/image-form-step";
 import {useState} from "react";
 import {PromptInfo} from "@uml2code/view/sequence-diagram-to-code";
-import {Autocomplete, Checkbox, FormControlLabel, TextField} from "@mui/material";
 import Grid from "@mui/system/Unstable_Grid";
-import ScienceIcon from '@mui/icons-material/Science';
 import Spinner from '@uml2code/components/spinner';
-
-type ProgramingLanguagesType = {
-    value: string;
-    frameworks: string[]
-}
-
-const PROGRAMING_LANGUAGES_LIST: readonly ProgramingLanguagesType[] = [
-    { value: 'Typescript', frameworks: ['React', 'VueJS', 'Angular', 'NestJS', 'NextJS', 'Express']},
-    { value: 'PHP', frameworks: ['Laravel', 'Symfony', 'CodeIgniter', 'Yii', 'Phalcon', 'CakePHP', 'Slim', 'Laminas', 'FuelPHP', 'Flight'] },
-    { value: 'Golang', frameworks: ['Gin', 'Echo', 'Beego', 'Revel', 'Fiber', 'Buffalo', 'Gorilla', 'Gorm', 'Iris', 'Goji'] },
-    { value: 'Java', frameworks: ['Spring Framework', 'Apache Struts', 'Apache Wicket', 'Play', 'Vaadin', 'Grails', 'Dropwizard', 'Vert.x'] },
-    { value: 'Ruby', frameworks: ['Ruby on Rails', 'Sinatra', 'Hanami', 'Padrino', 'Cuba', 'Merb', 'Grape', 'Camping', 'Ramaze', 'Nancy'] },
-    { value: 'Python', frameworks: ['Django', 'Flask', 'FastAPI', 'Pyramid', 'Bottle', 'CherryPy', 'Tornado', 'TurboGears', 'Web2py', 'Dash'] },
-];
-
-const SOFTWARE_ARCHITECTURES: readonly string[] = [
-    'Layered (N-Tier)',
-    'Microservice',
-    'Event Driven',
-    'Microkernel',
-    'Space-Based',
-    'Client-Server',
-    'Master-Slave',
-    'Pipe-Filter',
-    'Broker',
-    'Peer-to-Peer'
-]
+import SetupFormStep from "@uml2code/components/form-steps/setup-form-step";
 
 interface ImageFormStepProps {
     setGeneratedCode: (value: string) => void;
@@ -189,90 +161,17 @@ export default function FormStepper({ setGeneratedCode, setPrompt }: ImageFormSt
             {
                 activeStep === 1 && (
                     <>
-                        <Grid container spacing={2}>
-                            <Box sx={{ width: '100%', alignItems: 'center'}}>
-                                <Grid xs={12} md={12} style={{ textAlign: 'center', alignItems: 'center' }}>
-                                    <Typography sx={{ mt: 2, mb: 1, textAlign: 'left'}} variant="subtitle1">
-                                        Select your favourite programming language, framework and software architecture ⬇️
-                                    </Typography>
-                                    <Grid container spacing={2}>
-                                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
-                                            <Autocomplete
-                                                autoComplete
-                                                disablePortal
-                                                id="programming-language"
-                                                color="secondary"
-                                                disabled={isGeneratingCode}
-                                                options={PROGRAMING_LANGUAGES_LIST.map((value) => value.value)}
-                                                getOptionLabel={(option) => option}
-                                                sx={{ width: '100%' }}
-                                                renderInput={(params) =>
-                                                    <TextField {...params} color="primary" label="Programming language" />
-                                                }
-                                                value={programmingLanguage}
-                                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
-                                                    setProgrammingLanguage(newValue);
-                                                }}
-                                            />
-                                        </Grid>
-                                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
-                                            <Autocomplete
-                                                autoComplete
-                                                disablePortal
-                                                id="framework"
-                                                color="secondary"
-                                                disabled={isGeneratingCode}
-                                                options={PROGRAMING_LANGUAGES_LIST.find((value) => value.value === programmingLanguage)?.frameworks ?? []}
-                                                getOptionLabel={(option) => option}
-                                                sx={{ width: '100%' }}
-                                                renderInput={(params) =>
-                                                    <TextField {...params} color="primary" label="Frameworks" />
-                                                }
-                                                value={framework}
-                                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
-                                                    setFramework(newValue);
-                                                }}
-                                            />
-                                        </Grid>
-                                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
-                                            <Autocomplete
-                                                autoComplete
-                                                disablePortal
-                                                id="software-architecture"
-                                                color="secondary"
-                                                disabled={isGeneratingCode}
-                                                options={SOFTWARE_ARCHITECTURES}
-                                                getOptionLabel={(option) => option}
-                                                sx={{ width: '100%' }}
-                                                renderInput={(params) =>
-                                                    <TextField {...params} color="primary" label="Software architecture" />
-                                                }
-                                                value={architecture}
-                                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
-                                                    setArchitecture(newValue);
-                                                }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                                <Grid xs={12} md={12} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center' }}>
-                                    <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
-                                        <FormControlLabel
-                                            control={
-                                                <Checkbox
-                                                    checked={wantTest}
-                                                    onChange={() => setWantTest((prev) => !prev)}
-                                                    color="success"
-                                                    checkedIcon={<ScienceIcon />}
-                                                />
-                                            }
-                                            disabled={isGeneratingCode}
-                                            label="Would you like to add tests?"
-                                        />
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Grid>
+                        <SetupFormStep
+                            isGeneratingCode={isGeneratingCode}
+                            programmingLanguage={programmingLanguage}
+                            framework={framework}
+                            architecture={architecture}
+                            wantTest={wantTest}
+                            setProgrammingLanguage={setProgrammingLanguage}
+                            setFramework={setFramework}
+                            setArchitecture={setArchitecture}
+                            setWantTest={setWantTest}
+                        />
                     </>
                 )
             }
---
+++ src/components/form-steps/setup-form-step.tsx
@@ -0,0 +1,135 @@
+import Box from "@mui/material/Box";
+import Grid from "@mui/system/Unstable_Grid";
+import Typography from "@mui/material/Typography";
+import {Autocomplete, Checkbox, FormControlLabel, TextField} from "@mui/material";
+import * as React from "react";
+import ScienceIcon from "@mui/icons-material/Science";
+
+
+type ProgramingLanguagesType = {
+    value: string;
+    frameworks: string[]
+}
+
+const PROGRAMING_LANGUAGES_LIST: readonly ProgramingLanguagesType[] = [
+    { value: 'Typescript', frameworks: ['React', 'VueJS', 'Angular', 'NestJS', 'NextJS', 'Express']},
+    { value: 'PHP', frameworks: ['Laravel', 'Symfony', 'CodeIgniter', 'Yii', 'Phalcon', 'CakePHP', 'Slim', 'Laminas', 'FuelPHP', 'Flight'] },
+    { value: 'Golang', frameworks: ['Gin', 'Echo', 'Beego', 'Revel', 'Fiber', 'Buffalo', 'Gorilla', 'Gorm', 'Iris', 'Goji'] },
+    { value: 'Java', frameworks: ['Spring Framework', 'Apache Struts', 'Apache Wicket', 'Play', 'Vaadin', 'Grails', 'Dropwizard', 'Vert.x'] },
+    { value: 'Ruby', frameworks: ['Ruby on Rails', 'Sinatra', 'Hanami', 'Padrino', 'Cuba', 'Merb', 'Grape', 'Camping', 'Ramaze', 'Nancy'] },
+    { value: 'Python', frameworks: ['Django', 'Flask', 'FastAPI', 'Pyramid', 'Bottle', 'CherryPy', 'Tornado', 'TurboGears', 'Web2py', 'Dash'] },
+];
+
+const SOFTWARE_ARCHITECTURES: readonly string[] = [
+    'Layered (N-Tier)',
+    'Microservice',
+    'Event Driven',
+    'Microkernel',
+    'Space-Based',
+    'Client-Server',
+    'Master-Slave',
+    'Pipe-Filter',
+    'Broker',
+    'Peer-to-Peer'
+]
+
+interface SetupFormStepProps {
+    isGeneratingCode: boolean;
+    programmingLanguage: string|null;
+    framework: string|null;
+    architecture: string|null;
+    wantTest: boolean;
+    setProgrammingLanguage: (value: string|null) => void;
+    setFramework: (value: string|null) => void;
+    setArchitecture: (value: string|null) => void;
+    setWantTest: (prev: (prev: boolean) => boolean) => void;
+}
+
+export default function SetupFormStep({ isGeneratingCode, programmingLanguage, framework, architecture, wantTest, setProgrammingLanguage, setFramework, setArchitecture, setWantTest }: SetupFormStepProps) {
+    return (
+        <Grid container spacing={2}>
+            <Box sx={{ width: '100%', alignItems: 'center'}}>
+                <Grid xs={12} md={12} style={{ textAlign: 'center', alignItems: 'center' }}>
+                    <Typography sx={{ mt: 2, mb: 1, textAlign: 'left'}} variant="subtitle1">
+                        Select your favourite programming language, framework and software architecture ⬇️
+                    </Typography>
+                    <Grid container spacing={2}>
+                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
+                            <Autocomplete
+                                autoComplete
+                                disablePortal
+                                id="programming-language"
+                                color="secondary"
+                                disabled={isGeneratingCode}
+                                options={PROGRAMING_LANGUAGES_LIST.map((value) => value.value)}
+                                getOptionLabel={(option) => option}
+                                sx={{ width: '100%' }}
+                                renderInput={(params) =>
+                                    <TextField {...params} color="primary" label="Programming language" />
+                                }
+                                value={programmingLanguage}
+                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
+                                    setProgrammingLanguage(newValue);
+                                }}
+                            />
+                        </Grid>
+                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
+                            <Autocomplete
+                                autoComplete
+                                disablePortal
+                                id="framework"
+                                color="secondary"
+                                disabled={isGeneratingCode}
+                                options={PROGRAMING_LANGUAGES_LIST.find((value) => value.value === programmingLanguage)?.frameworks ?? []}
+                                getOptionLabel={(option) => option}
+                                sx={{ width: '100%' }}
+                                renderInput={(params) =>
+                                    <TextField {...params} color="primary" label="Frameworks" />
+                                }
+                                value={framework}
+                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
+                                    setFramework(newValue);
+                                }}
+                            />
+                        </Grid>
+                        <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
+                            <Autocomplete
+                                autoComplete
+                                disablePortal
+                                id="software-architecture"
+                                color="secondary"
+                                disabled={isGeneratingCode}
+                                options={SOFTWARE_ARCHITECTURES}
+                                getOptionLabel={(option) => option}
+                                sx={{ width: '100%' }}
+                                renderInput={(params) =>
+                                    <TextField {...params} color="primary" label="Software architecture" />
+                                }
+                                value={architecture}
+                                onChange={(_event: React.SyntheticEvent<Element, Event>, newValue: string | null) => {
+                                    setArchitecture(newValue);
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid xs={12} md={12} style={{ display: 'flex', flexDirection: 'column', textAlign: 'center', alignItems: 'center' }}>
+                    <Grid xs={12} md={4} style={{ textAlign: 'center', alignItems: 'center' }}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={wantTest}
+                                    onChange={() => setWantTest((prev: boolean) => !prev)}
+                                    color="success"
+                                    checkedIcon={<ScienceIcon />}
+                                />
+                            }
+                            disabled={isGeneratingCode}
+                            label="Would you like to add tests?"
+                        />
+                    </Grid>
+                </Grid>
+            </Box>
+        </Grid>
+    )
+}
