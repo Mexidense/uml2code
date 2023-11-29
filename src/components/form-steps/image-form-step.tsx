34fb289390490cbafacff247ca33e92b95ef1f90
Salvador Briones <mexidense@gmail.com>
2023-11-29 21:00:19 +0100

    feat: reset image

--- src/components/form-steps/image-form-step.tsx
+++ src/components/form-steps/image-form-step.tsx
@@ -3,9 +3,11 @@ import {useState} from "react";
 import {ImageFileUploader} from "@uml2code/components/image-file-uploader";
 import Grid from "@mui/system/Unstable_Grid";
 import {Box, Card, CardActionArea, CardMedia} from "@mui/material";
+import Button from "@mui/material/Button";
+import * as React from "react";
 
 interface ImageFormStepProps {
-    setUploadedImage: (value: string) => void;
+    setUploadedImage: (value: string|null) => void;
     imageSource: string|null;
 }
 
@@ -17,27 +19,37 @@ export function ImageFormStep({ setUploadedImage, imageSource }: ImageFormStepPr
         setImage(imageSource);
     };
 
+    const handleResetImage = () => {
+        setUploadedImage(null)
+        setImage(null);
+    }
+
+
     return (
         <>
             <Box sx={{ width: '100%', alignItems: 'center'}}>
                 <Grid container spacing={2}>
                     <Grid xs={12} md={12} style={{ textAlign: 'center', alignItems: 'center' }}>
                         {
-                            !image &&
-                            // <ImageFileUploader onCodeUpdate={handleCodeUpdate} setLoading={setLoading} onImageSourceUpdate={handleImageUpdate}/>
-                            <ImageFileUploader setUploadedImage={handleImageUpdate}/>
+                            !image && (
+                                <ImageFileUploader setUploadedImage={handleImageUpdate}/>
+                            )
                         }
                         {image &&
                             <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                                <Card sx={{ maxWidth: 200 }}>
+                                <Card>
                                     <CardActionArea>
                                         <CardMedia
                                             component="img"
-                                            width="100%"
+                                            height={300}
                                             image={image}
-                                            alt="Image uploaded"
+                                            alt="UML sequence diagram"
+                                            sx={{ padding: "1em 1em 0 1em", objectFit: 'cover' }}
                                         />
                                     </CardActionArea>
+                                    <Button onClick={handleResetImage}>
+                                        Change image
+                                    </Button>
                                 </Card>
                             </Box>
                         }
