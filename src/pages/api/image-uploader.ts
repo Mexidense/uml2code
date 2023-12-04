c296fd9bb33a4ed2f84829d8727047a7c9fcd2cd
Salvador Briones <mexidense@gmail.com>
2023-12-05 00:43:58 +0100

    refactor: move image transformation to frontend

--- src/pages/api/image-uploader.ts
+++ src/pages/api/image-uploader.ts
@@ -2,16 +2,13 @@ import { NextApiRequest, NextApiResponse } from "next";
 import {
     GenerateCodeFromSequenceDiagram, GenerateCodeFromSequenceDiagramResponse
 } from "@uml2code/back-end/generate-code/generate-code-from-sequence-diagram.service";
-import {ImageTransformationService} from "@uml2code/back-end/image-transformation/image-transformation.service";
 
 export default async function POST (apiRequest: NextApiRequest, apiResponse: NextApiResponse) {
     try {
         const { image, programmingLanguage, framework, architecture, shouldHasTests} = apiRequest.body;
 
-        const transformedImage = await ImageTransformationService.transform(image);
-
         const request = {
-            image: transformedImage,
+            image: image,
             programmingLanguage: programmingLanguage,
             framework: framework,
             architecture: architecture,
