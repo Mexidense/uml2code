6022d01a8396dbafbe37507fab224b65334eb6e1
Salvador Briones <mexidense@gmail.com>
2023-12-12 21:38:00 +0100

    chore: set Google Cloud RUN as CI/CD

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,12 @@ ARG PUBLIC_APP_URL
 
 FROM node:18.17.0-alpine
 RUN mkdir /app
-COPY package.json /app/
+
 WORKDIR /app
 
-COPY . ./
+COPY . .
 
-ENV NEXT_PUBLIC_APP_URL ${PUBLIC_APP_URL}
-
-RUN npm ci
+RUN npm ci --omit=dev
 RUN npm run build
 
-EXPOSE 3000
-
-CMD ["npm", "run","start"]
+CMD ["npm", "start"]
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path')
+
+const nextConfig = {
+    output: 'standalone',
+    webpack: (config, { _isServer }) => {
+        config.resolve.alias['@uml2code'] = path.resolve(__dirname, './src/');
+
+        return config;
+    },
+}
 
 module.exports = nextConfig
